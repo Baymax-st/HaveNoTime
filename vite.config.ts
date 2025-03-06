7d62619166df76a4c0c41c7d1bf344d280a0f2bd
baymax <xs_work004@163.com>
2025-03-06 21:23:13 +0800

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+// 整个工程的配置文件，可以安装插件
+
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
